1ce157158ac9b7f5a4089788d29d66307171e9a3
arizalinside <arizalinc@gmail.com>
2021-06-30 03:19:11 +0700

    add seeder and faker for generate data dummy

---
+++ database/seeders/PegawaiSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use Faker\Factory as Faker;
+
+class PegawaiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+
+        for ($i = 1; $i <= 50; $i++) {
+
+            // insert data ke table pegawai
+            DB::table('pegawai')->insert([
+                'pegawai_nama' => $faker->name,
+                'pegawai_jabatan' => $faker->jobTitle,
+                'pegawai_umur' => $faker->numberBetween(25, 50),
+                'pegawai_alamat' => $faker->address
+            ]);
+        }
+    }
+}
