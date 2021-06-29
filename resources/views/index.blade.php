a5024c3854f873238776d318579af353f03a6c9a
arizalinside <arizalinc@gmail.com>
2021-06-30 05:40:32 +0700

    styling page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,63 +6,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel CRUD</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
-
-    <style type="text/css">
-        .pagination li {
-            float: left;
-            list-style-type: none;
-            margin: 5px;
-        }
-    </style>
-
-    <h3>Data Pegawai</h3>
-
-    <form>
+    <div class="container-fluid">
+        <a href="/pegawai" style="text-decoration: none; color: inherit;">
+            <h1 class="text-center mt-3 font-weight-bold">DATA PEGAWAI</h1>
+        </a>
+        <!-- <form>
         <button formaction="/pegawai/tambah">+ Tambah Pegawai Baru</button>
-    </form>
+    </form> -->
+        <div class="d-flex justify-content-between">
+            <a class="btn btn-primary font-weight-bold" href="/pegawai/tambah" style="margin: 2rem;">+ Tambah Pegawai Baru
+            </a>
 
-    <p>Pencarian:</p>
-    <form action="/pegawai/cari" method="GET">
-        <input type="text" name="cari" placeholder="Cari Pegawai..." value="{{ old('cari') }}">
-        <input type="submit" value="Search">
-    </form>
+            <div class="m-3">
+                <p class="font-weight-bold m-0">Pencarian:</p>
+                <form action="/pegawai/cari" method="GET">
+                    <input type="text" name="cari" placeholder="Cari Pegawai..." value="{{ old('cari') }}">
+                    <input class="btn btn-info btn-sm m-1" type="submit" value="Search">
+                </form>
+            </div>
+        </div>
 
-    <br>
-    <br>
+        <table class="table table-bordered table-striped">
+            <tr class="text-center bg-warning">
+                <th>Nama</th>
+                <th>Jabatan</th>
+                <th>Umur</th>
+                <th>Alamat</th>
+                <th>Opsi</th>
+            </tr>
+            @foreach($pegawai as $pgw)
+            <tr>
+                <td>{{ $pgw->pegawai_nama }}</td>
+                <td>{{ $pgw->pegawai_jabatan }}</td>
+                <td>{{ $pgw->pegawai_umur }}</td>
+                <td>{{ $pgw->pegawai_alamat }}</td>
+                <td>
+                    <a class="btn btn-info btn-sm" href="/pegawai/edit/{{ $pgw->pegawai_id }}">Edit</a>
+                    <a class="btn btn-danger btn-sm" href="/pegawai/hapus/{{ $pgw->pegawai_id }}">Hapus</a>
+                </td>
+            </tr>
+            @endforeach
+        </table>
 
-    <table border="1">
-        <tr>
-            <th>Nama</th>
-            <th>Jabatan</th>
-            <th>Umur</th>
-            <th>Alamat</th>
-            <th>Opsi</th>
-        </tr>
-        @foreach($pegawai as $pgw)
-        <tr>
-            <td>{{ $pgw->pegawai_nama }}</td>
-            <td>{{ $pgw->pegawai_jabatan }}</td>
-            <td>{{ $pgw->pegawai_umur }}</td>
-            <td>{{ $pgw->pegawai_alamat }}</td>
-            <td>
-                <a href="/pegawai/edit/{{ $pgw->pegawai_id }}">Edit</a>
-                <a href="/pegawai/hapus/{{ $pgw->pegawai_id }}">Hapus</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
-
-    <br>
-    Halaman: {{ $pegawai->currentPage() }} <br>
-    Jumlah data: {{ $pegawai->total() }} <br>
-    Data per halaman: {{ $pegawai->perPage() }} <br>
-
-    {{ $pegawai->links('pagination::bootstrap-4') }}
-
-
+        <br>
+        <div class="text-center mb-3">
+            Halaman: {{ $pegawai->currentPage() }} <br>
+            Jumlah data: {{ $pegawai->total() }} <br>
+            Data per halaman: {{ $pegawai->perPage() }} <br>
+        </div>
+        <div class="d-flex justify-content-center pagination-lg">
+            {{ $pegawai->links('pagination::bootstrap-4') }}
+        </div>
+    </div>
 </body>
 
 </html>
